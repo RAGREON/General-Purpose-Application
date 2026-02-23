23e954db4d66259b4a6f9046ffa13227ee994084
RAGREON <ragreon809@gmail.com>
2026-02-23 09:51:19 +0545

    migrate(lab): add shadow properties

--- backend/Lab/Data/Migrations/20260222131316_AddLabShadowProperties.Designer.cs
+++ backend/Lab/Data/Migrations/20260222131316_AddLabShadowProperties.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Lab.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Lab.Data.Migrations
 {
     [DbContext(typeof(LabContext))]
-    partial class LabContextModelSnapshot : ModelSnapshot
+    [Migration("20260222131316_AddLabShadowProperties")]
+    partial class AddLabShadowProperties
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
