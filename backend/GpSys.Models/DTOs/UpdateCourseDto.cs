51c752e89f5b65c88999875929e5aedb369e1eb8
RAGREON <ragreon809@gmail.com>
2026-02-24 22:53:59 +0545

    Revert "feat[models(course)]: add course models and dtos"
    This reverts commit 3614a4e6986b18c96a36e671d6ab9fd2f5b9692a.

--- backend/GpSys.Models/DTOs/UpdateCourseDto.cs
+++
@@ -1,9 +0,0 @@
-namespace GpSys.Models.DTOs
-{
-  public class UpdateCourseDto
-  {
-    public string? Code { get; set; }
-    public string? Title { get; set; }
-    public string? Alias { get; set; }
-  }
-}
