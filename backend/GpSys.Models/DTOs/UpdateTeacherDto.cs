e44c19c1ee65404df15f666c30eec81f7876a313
RAGREON <ragreon809@gmail.com>
2026-02-25 00:35:26 +0545

    feat[models(teacher)]: add teacher entity and dtos

---
+++ backend/GpSys.Models/DTOs/UpdateTeacherDto.cs
@@ -0,0 +1,8 @@
+namespace GpSys.Models.DTOs
+{
+  public class UpdateTeacherDto
+  {
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+  }
+}
---
+++ backend/GpSys.Models/Entities/Teacher.cs
@@ -0,0 +1,9 @@
+namespace GpSys.Models.Entities
+{
+  public class TeacherEntity
+  {
+    public int Id { get; set; }
+    public string FirstName { get; set; } = null!;
+    public string LastName { get; set; } = null!;
+  }
+}
