ad5da9c77298a7d81362c68f80f9d42efdc05441
RAGREON <ragreon809@gmail.com>
2026-02-25 00:58:44 +0545

    feat: register TeacherContext

--- backend/GpSys.Teacher/Program.cs
+++ backend/GpSys.Teacher/Program.cs
@@ -2,6 +2,11 @@ var builder = WebApplication.CreateBuilder();
 
 string? connectionString = builder.Configuration.GetConnectionString("DefaultString");
 
+builder.Services.AddDbContext<TeacherContext>(options =>
+{
+  options.UseNpgsql(connectionString);
+});
+
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
