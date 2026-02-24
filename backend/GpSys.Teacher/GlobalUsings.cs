01ff0f3a5b5fb581739dc91364a6a8b88483510f
RAGREON <ragreon809@gmail.com>
2026-02-25 00:57:15 +0545

    chore: add global references

---
+++ backend/GpSys.Teacher/GlobalUsings.cs
@@ -0,0 +1,8 @@
+global using GpSys.Models.Entities;
+global using GpSys.Models.DTOs;
+global using GpSys.Teacher.Data;
+global using GpSys.Teacher.Interfaces;
+global using GpSys.Teacher.Repositories;
+
+global using Microsoft.AspNetCore.Mvc;
+global using Microsoft.EntityFrameworkCore;
