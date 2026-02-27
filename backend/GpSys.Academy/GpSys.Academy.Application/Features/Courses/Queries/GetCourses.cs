313f080c7a2748766b78a1e037d5d54fc4d62cdb
RAGREON <ragreon809@gmail.com>
2026-02-27 23:39:22 +0545

    chore: fixed typo

--- backend/GpSys.Academy/GpSys.Academy.Application/Features/Courses/Queries/GetCourses.cs
+++ backend/GpSys.Academy/GpSys.Academy.Application/Features/Courses/Queries/GetCourses.cs
@@ -1,13 +1,13 @@
 namespace GpSys.Academy.Application.Features.Courses
 {
-  public record GetCoursesCommand() : IRequest<IList<CourseDto>>;
+  public record GetCoursesQuery() : IRequest<IList<CourseDto>>;
 
   public class GetCoursesHandler(ApplicationDbContext context) 
-    : IRequestHandler<GetCoursesCommand, IList<CourseDto>>
+    : IRequestHandler<GetCoursesQuery, IList<CourseDto>>
   {
     private readonly ApplicationDbContext _context = context;
 
-    public async Task<IList<CourseDto>> Handle(GetCoursesCommand command, CancellationToken token)
+    public async Task<IList<CourseDto>> Handle(GetCoursesQuery command, CancellationToken token)
     {
       var courses = await _context.Courses
         .Select(c => new CourseDto(
