1c67c102b174f7622ef1eb5f0b65844b9919b005
RAGREON <ragreon809@gmail.com>
2026-03-02 01:06:36 +0545

    chore: add global usings, and references to .csproj

--- backend/GpSys.Academy/src/GpSys.Academy.Application/GlobalUsings.cs
+++ backend/GpSys.Academy/src/GpSys.Academy.Application/GlobalUsings.cs
@@ -1,5 +1,6 @@
+global using GpSys.Academy.Domain.Common;
 global using GpSys.Academy.Domain.Entities;
-global using GpSys.Academy.Infrastructure.Persistence;
+global using GpSys.Academy.Application.Common.Interfaces;
 
 global using MediatR;
 global using Microsoft.EntityFrameworkCore;
