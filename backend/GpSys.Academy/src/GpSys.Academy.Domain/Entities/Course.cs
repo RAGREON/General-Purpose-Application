9d2a461eede6f42c4df7af20e3b29788cf6c080b
RAGREON <ragreon809@gmail.com>
2026-03-02 00:40:56 +0545

    feat[Domain(Entity)]: create Factory methods and integrate Result pattern"

--- backend/GpSys.Academy/src/GpSys.Academy.Domain/Entities/Course.cs
+++ backend/GpSys.Academy/src/GpSys.Academy.Domain/Entities/Course.cs
@@ -10,18 +10,49 @@ namespace GpSys.Academy.Domain.Entities
 
     private Course() { }
 
-    public Course(string code, string title, string alias)
+    private Course(string code, string title, string alias)
     {
       Code = code;
       Title = title;
       Alias = alias;
     }
 
-    public void Update(string? code, string? title, string? alias)
+    public static Result<Course> Create(string code, string title, string alias)
     {
+      IList<Error> errors = [];
+
+      if (string.IsNullOrWhiteSpace(code))
+        errors.Add(new Error("EMPTY_FIELD", "Course code is required."));
+      
+      if (string.IsNullOrWhiteSpace(title))
+        errors.Add(new Error("EMPTY_FIELD", "Course title is required"));
+
+      if (errors.Any()) 
+        return Result<Course>.Failure(errors);
+
+      var course = new Course(code, title, alias);
+
+      return Result<Course>.Success(course);
+    }
+
+    public Result<Course> Update(string? code, string? title, string? alias)
+    {
+      IList<Error> errors = [];
+
+      if (code is { Length: 0 } || (code != null && string.IsNullOrWhiteSpace(code)))
+        errors.Add(new Error("EMPTY_FIELD", "Course Code cannot be cleared"));
+
+      if (title is { Length: 0 } || (title != null && string.IsNullOrWhiteSpace(title)))
+        errors.Add(new Error("EMPTY_FIELD", "Course title cannot be cleared.")); 
+
+      if (errors.Any()) 
+        return Result<Course>.Failure(errors);
+
       if (!string.IsNullOrWhiteSpace(code)) Code = code; 
       if (!string.IsNullOrWhiteSpace(title)) Title = title; 
       if (!string.IsNullOrWhiteSpace(alias)) Alias = alias; 
+
+      return Result<Course>.Success(this);
     }
   }
 }
