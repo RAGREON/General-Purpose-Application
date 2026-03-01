dd30a520b844ca2410a04e8ad244f778d62a1fb8
RAGREON <ragreon809@gmail.com>
2026-03-02 00:33:50 +0545

    feat[Persistence]: inherit IApplicationDbContext and integrate AuditableEntityInterceptor

--- backend/GpSys.Academy/src/GpSys.Academy.Infrastructure/Persistence/ApplicationDbContext.cs
+++ backend/GpSys.Academy/src/GpSys.Academy.Infrastructure/Persistence/ApplicationDbContext.cs
@@ -1,7 +1,24 @@
+using GpSys.Academy.Application.Common.Interfaces;
+using GpSys.Academy.Infrastructure.Data.Interceptors;
+
 namespace GpSys.Academy.Infrastructure.Persistence
 {
-  public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options) 
+  public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options), IApplicationDbContext 
   {
     public DbSet<Course> Courses => Set<Course>();
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+      optionsBuilder.AddInterceptors(new AuditableEntityInterceptor());
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+      base.OnModelCreating(modelBuilder);
+
+      modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+
+      modelBuilder.Entity<Course>().HasQueryFilter(c => !c.IsDeleted);
+    }
   }
 }
