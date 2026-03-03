1c956886a5a859ad1a9f8108931660a6e9bce64b
RAGREON <ragreon809@gmail.com>
2026-03-03 16:11:23 +0545

    chore!: remove legacy GpSys.Teacher project

--- backend/GpSys.Teacher/Data/TeacherContext.cs
+++
@@ -1,7 +0,0 @@
-namespace GpSys.Teacher.Data
-{
-  public class TeacherContext(DbContextOptions options) : DbContext(options)
-  {
-    public DbSet<TeacherEntity> Teachers => Set<TeacherEntity>();
-  }
-}
--- backend/GpSys.Teacher/GlobalUsings.cs
+++
@@ -1,8 +0,0 @@
-global using GpSys.Models.Entities;
-global using GpSys.Models.DTOs;
-global using GpSys.Teacher.Data;
-global using GpSys.Teacher.Interfaces;
-global using GpSys.Teacher.Repositories;
-
-global using Microsoft.AspNetCore.Mvc;
-global using Microsoft.EntityFrameworkCore;
